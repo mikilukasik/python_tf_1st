7fbc3516fc45fc0acd6d22de9a8916f0586bc6ec
mikilukasik <szarhazi@gmail.com>
2023-02-26 20:31:05 +0000

    .

--- scripts/trainModel/train_c2d1_L_v1.py
+++ scripts/trainModel/train_c2d1_L_v1.py
@@ -1,7 +1,12 @@
 import tensorflow as tf
-from urllib.request import urlopen
+from keras.utils import to_categorical
+from urllib.request import urlopen, Request
 import json
 import numpy as np
+import gzip
+import pandas as pd
+
+# with tf.device('/cpu:0'):
 
 BATCH_SIZE = 32
 SHUFFLE_BUFFER_SIZE = 100
@@ -10,42 +15,34 @@ datasetReaderId = json.loads(
     urlopen("http://localhost:3500/datasetReader").read())["id"]
 print("datasetReaderId", datasetReaderId)
 
-model = tf.keras.models.load_model('./models/c2d1_L_v1/2.5178210735321045')
+model = tf.keras.models.load_model('./models/c2d1_L_v1/2.388428211212158')
 # model = tf.keras.models.load_model('./models/blanks/c2d1_L_v1')
 
-model.compile(optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.000001), loss='categorical_crossentropy',
+model.compile(optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.0000005), loss='categorical_crossentropy',
               metrics=['categorical_crossentropy'])
 
 model.summary()
 
-# testDataset = json.loads(
-#     urlopen("http://localhost:3500/datasetReader/" + datasetReaderId + "/testDataset").read())
-
-# print("testDataset length", len(testDataset['xs']))
-
-# tdst = tf.reshape(tf.constant(
-#     np.asanyarray(testDataset["xs"])), [-1, 8, 8, 14])
-# testDatasetTensor = tf.data.Dataset.from_tensor_slices(
-#     (tdst, np.asanyarray(testDataset["ys"])))
-
-# testDatasetTensor = testDatasetTensor.batch(BATCH_SIZE)
-
 for x in range(10000):
-    for x in range(30):
-        dataset = json.loads(
-            urlopen("http://localhost:3500/datasetReader/" + datasetReaderId + "/dataset").read())
+    for y in range(30):
 
-        print("loaded dataset length", len(dataset['xs']))
+        datasetCsv = pd.read_csv(
+            "http://localhost:3500/datasetReader/" + datasetReaderId + "/dataset?format=csv", header=None)
+        print("loaded dataset.", y)
+
+        dataset_features = datasetCsv.copy()
+        dataset_labels = dataset_features.pop(896)
+
+        dataset_features = np.array(dataset_features)
+        dataset_labels = to_categorical(dataset_labels, num_classes=1837)
 
         datasetTensor = tf.data.Dataset.from_tensor_slices((tf.reshape(tf.constant(
-            np.asarray(dataset["xs"])), [-1, 8, 8, 14]), np.asarray(dataset["ys"])))
+            dataset_features), [-1, 8, 8, 14]), dataset_labels))
 
         datasetTensor = datasetTensor.shuffle(
             SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
 
         fitResult = model.fit(datasetTensor, epochs=1)
 
-        print(fitResult.history["loss"][0])
-
     model.save('./models/c2d1_L_v1/'+str(fitResult.history["loss"][0]))
-    print('model saved.')
+    print('* * * * * * model saved.', fitResult.history["loss"][0])
