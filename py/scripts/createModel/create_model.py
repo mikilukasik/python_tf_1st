1cd755c44ed471b4539f118d2c5efccfe8f49f7e
mikilukasik <szarhazi@gmail.com>
2023-03-02 18:14:36 +0000

    .

--- py/scripts/createModel/create_model.py
+++ py/scripts/createModel/create_model.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import List, Tuple
 from keras.models import Model
 from keras.layers import Input
 from keras.layers import Dense, Conv2D, ELU, Flatten, Concatenate, Add
@@ -7,8 +7,9 @@ from save_model import save_model
 
 def create_chess_model(model_name: str,
                        input_shape: Tuple[int, int, int] = (8, 8, 14),
-                       filters: Tuple[int, int, int, int] = (
-                           64, 128, 256, 512),
+                       conv_blocks: List[Tuple[int, Tuple[int, int], Tuple[int, ...]]] = [
+                           (4, (3, 3), (64, 128, 256, 512))],
+                       activation: str = 'relu',
                        dense_units: Tuple[int, int] = (1024, 512),
                        output_units: int = 1837) -> Model:
     """
@@ -17,7 +18,12 @@ def create_chess_model(model_name: str,
     Parameters:
     - model_name: A string representing the name of the model.
     - input_shape: Tuple of 3 integers representing the shape of input data. Default is (8,8,14).
-    - filters: Tuple of 4 integers representing the number of filters for convolutional layers. Default is (64,128,256,512).
+    - conv_blocks: List of tuples, where each tuple represents a convolutional block. 
+      The tuple contains:
+      - The number of convolutional layers in the block.
+      - The size of the convolutional kernel.
+      - The number of filters for each convolutional layer.
+    - activation: String representing the activation function to use. Default is 'relu'.
     - dense_units: Tuple of 2 integers representing the number of units for dense layers. Default is (1024,512).
     - output_units: Integer representing the number of output units. Default is 1837.
 
@@ -28,32 +34,34 @@ def create_chess_model(model_name: str,
     input = Input(shape=input_shape)
 
     # Convolutional layers
-    x = Conv2D(filters[0], (3, 3), padding='same', activation='relu')(input)
-    y = Conv2D(filters[0], (3, 3), padding='same', activation='relu')(x)
-    z = Add()([x, y])
+    prev_outputs = []
+    for i, block in enumerate(conv_blocks):
+        num_layers, kernel_size, filters = block
+        prev_output = input
+        for j in range(num_layers):
+            x = Conv2D(filters[j], kernel_size, padding='same',
+                       activation=activation)(prev_output)
+            y = Conv2D(filters[j], kernel_size,
+                       padding='same', activation=activation)(x)
+            z = Add()([x, y])
+            prev_output = z
+        prev_outputs.append(prev_output)
 
-    x = Conv2D(filters[1], (3, 3), padding='same', activation='relu')(z)
-    y = Conv2D(filters[1], (3, 3), padding='same', activation='relu')(x)
-    z = Add()([x, y])
-
-    x = Conv2D(filters[2], (3, 3), padding='same', activation='relu')(z)
-    y = Conv2D(filters[2], (3, 3), padding='same', activation='relu')(x)
-    z = Add()([x, y])
-
-    x = Conv2D(filters[3], (3, 3), padding='same', activation='relu')(z)
-    y = Conv2D(filters[3], (3, 3), padding='same', activation='relu')(x)
-    z = Add()([x, y])
+    if len(prev_outputs) > 1:
+        merged = Concatenate()(prev_outputs)
+    else:
+        merged = prev_outputs[0]
 
     flatInput = Flatten()(input)
-    flatConv = Flatten()(z)
+    flatConv = Flatten()(merged)
 
-    concatted1 = Concatenate()([flatInput,  flatConv])
+    concatted1 = Concatenate()([flatInput, flatConv])
     dense1 = Dense(dense_units[0], ELU())(concatted1)
 
-    concatted2 = Concatenate()([flatInput,  dense1])
+    concatted2 = Concatenate()([flatInput, dense1])
     dense2 = Dense(dense_units[1], ELU())(concatted2)
 
-    concatted3 = Concatenate()([flatInput,  dense2])
+    concatted3 = Concatenate()([flatInput, dense2])
 
     output = Dense(output_units, 'softmax')(concatted3)
 
