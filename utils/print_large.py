d98545c6e8d06a998396951e3b811347d19a1f48
mikilukasik <szarhazi@gmail.com>
2023-03-03 17:21:47 +0000

    .

--- utils/print_large.py
+++ utils/print_large.py
@@ -14,8 +14,8 @@ def print_large(*args, indent=30):
     # Calculate the width and height of the box
     box_width = len(longest) + 13 + (len(longest) % 2)
     # Define the top and bottom borders
-    top_bottom_border = ('*' * (box_width))
-    left_padding = ' ' * (indent)
+    top_bottom_border = '*' * box_width
+    left_padding = ' ' * indent
     # Print the top border
     print(f'{left_padding}{top_bottom_border}')
     # Print the sides with text and padding
@@ -23,6 +23,6 @@ def print_large(*args, indent=30):
         arg_str = str(arg)
         arg_padding = ' ' * (((box_width - 2) - len(arg_str)) // 2)
         print(
-            f'{left_padding}*{arg_padding}{arg_str}{arg_padding}{" " if (len(arg_str)) % 2 == 0 else ""}*')
+            f'{left_padding}*{arg_padding}{arg_str}{arg_padding}{" " if len(arg_str) % 2 == 0 else ""}*')
     # Print the bottom border
     print(f'{left_padding}{top_bottom_border}')
