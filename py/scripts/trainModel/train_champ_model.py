f98ced58bf02072a994d6654d6339524b627bce8
mikilukasik <szarhazi@gmail.com>
2023-03-13 19:40:33 +0000

    .

--- py/scripts/trainModel/train_champ_model.py
+++ py/scripts/trainModel/train_champ_model.py
@@ -1,13 +1,13 @@
 from utils.train_model_v4 import train_model
 
 # model_source = '../models/champ_he_M_2blocks/_blank'
-model_source = '../models/champ_he_M_2blocks/3.2868399620056152'
+model_source = '../models/champ_he_M_2blocks/1.8305308486620584'
 model_dest = '../models/champ_he_M_2blocks'
 
 initial_lr = 0.00001
 # initial_lr = 0.0001
 initial_batch_size = 64
-lr_multiplier = 3
+lr_multiplier = 0.3
 
 train_model(model_source, model_dest,
             initial_batch_size, initial_lr, gpu=True, lr_multiplier=lr_multiplier)
