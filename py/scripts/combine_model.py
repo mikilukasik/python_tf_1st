0c23dd8a34e4540ac7503d804a69fca313f39d2f
mikilukasik <szarhazi@gmail.com>
2023-04-01 01:18:49 +0000

    170m champ

--- py/scripts/combine_model.py
+++ py/scripts/combine_model.py
@@ -2,12 +2,12 @@ import keras
 from keras.models import model_from_json
 
 # Load the model structure from the JSON file
-with open("models/1.6615822938283287/model.json", "r") as json_file:
+with open("models/plain_16_v5/1.654546069463094/model.json", "r") as json_file:
     model_json = json_file.read()
 model = model_from_json(model_json)
 
 # Load the model weights from the .h5 file
-model.load_weights("models/1.6615822938283287/weights.h5")
+model.load_weights("models/plain_16_v5/1.654546069463094/weights.h5")
 
 # Save the combined model as a single Keras HDF5 model file
-model.save("models/1.6615822938283287/combined_model.h5")
+model.save("models/plain_16_v5/1.654546069463094/combined_model.h5")
